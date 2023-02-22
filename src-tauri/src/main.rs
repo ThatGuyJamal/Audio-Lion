6a9f09d50f412f7727b943ace2553c76d1259f0a
ThatGuyJamal <jamallyons8@gmail.com>
2023-02-21 19:46:04 -0500

    Add: Backend events to window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,48 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::api::shell;
+use tauri::{CustomMenuItem, Manager, Menu, MenuEntry, Submenu};
+
 fn main() {
+    let ctx = tauri::generate_context!();
+
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![])
-        .run(tauri::generate_context!())
+        .menu(Menu::with_items([
+            #[cfg(target_os = "windows")]
+            MenuEntry::Submenu(Submenu::new(
+                "Help",
+                Menu::with_items([
+                    CustomMenuItem::new("Discord Server", "Discord").into(),
+                    CustomMenuItem::new("Github Link", "Github").into(),
+                ]),
+            )),
+        ]))
+        .on_menu_event(|event| {
+            let event_name = event.menu_item_id();
+            match event_name {
+                "Github Link" => {
+                    let url = "https://github.com/ThatGuyJamal/Audio-Lion".to_string();
+                    shell::open(&event.window().shell_scope(), url, None).unwrap();
+                }
+                "Discord Server" => {
+                    let url = "https://discord.gg/MSTrBrNaGn".to_string();
+                    shell::open(&event.window().shell_scope(), url, None).unwrap();
+                }
+
+                _ => {}
+            }
+        })
+        .run(ctx)
         .expect("error while running tauri application");
 }
