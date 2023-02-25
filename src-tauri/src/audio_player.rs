c05a50d894e146af3b24a616a925ab622916e9de
ThatGuyJamal <jamallyons8@gmail.com>
2023-02-24 20:53:20 -0500

    Refactor rust files

--- src-tauri/src/audio_player.rs
+++ src-tauri/src/audio_player.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 pub mod core {
     use tauri::{Manager, App};
 
@@ -26,4 +28,54 @@ pub mod core {
     }
 }
 
-pub mod stream {}
+pub mod stream {
+    #[derive(PartialEq, Debug)]
+    pub enum AudioFileTypes {
+        MP3,
+        WAV,
+    }
+
+    impl AudioFileTypes {
+        /// Returns an `AudioFileTypes` object from a file extension
+        fn from_extension(extension: &str) -> Option<Self> {
+            match extension.to_lowercase().as_str() {
+                "mp3" => Some(Self::MP3),
+                "wav" => Some(Self::WAV),
+                _ => None,
+            }
+        }
+    }
+
+    /// Returns a vector of `PathBuf` objects for all audio files in the given directory
+    pub fn get_audio_files(dir: &str, file_type: AudioFileTypes) -> Vec<std::path::PathBuf> {
+        let mut audio_files = vec![];
+
+        if let Ok(entries) = std::fs::read_dir(dir) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    let path = entry.path();
+                    // Check if the path is a file
+                    if let Some(extension) = path.extension() {
+                        // Check if the file type is the same as the file type we're looking for
+                        if let Some(file_type_from_ext) =
+                            AudioFileTypes::from_extension(&extension.to_string_lossy())
+                        {
+                            // If the file type is the same as the file type we're looking for, add it to the vector
+                            if file_type_from_ext == file_type {
+                                audio_files.push(path.to_path_buf());
+                            } else {
+                                continue;
+                            }
+                        }
+                    } else {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        audio_files
+    }
+}
