07309922d592c71bb4fe672d3367739bce9e0ef1
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-02-27 14:12:25 -0500

    Update main.rs
    uncommenting code

--- db_test/src/main.rs
+++ db_test/src/main.rs
@@ -23,36 +23,36 @@ async fn main() {
     // Get a handle to a collection in the database.
     let collection = client.database("testing").collection::<Book>("books");
 
-    // custom_print("Starting to insert documents into the database...");
+    custom_print("Starting to insert documents into the database...");
 
-    // let books = vec![
-    //     Book {
-    //         title: "The Grapes of Wrath".to_string(),
-    //         author: "John Steinbeck".to_string(),
-    //     },
-    //     Book {
-    //         title: "To Kill a Mockingbird".to_string(),
-    //         author: "Harper Lee".to_string(),
-    //     },
-    // ];
+    let books = vec![
+        Book {
+            title: "The Grapes of Wrath".to_string(),
+            author: "John Steinbeck".to_string(),
+        },
+        Book {
+            title: "To Kill a Mockingbird".to_string(),
+            author: "Harper Lee".to_string(),
+        },
+    ];
 
-    // collection.insert_many(books, None).await.unwrap();
+    collection.insert_many(books, None).await.unwrap();
 
-    // custom_print("Finished inserting documents into the database!");
+    custom_print("Finished inserting documents into the database!");
 
-    // custom_print("Starting to query the database...");
+    custom_print("Starting to query the database...");
 
-    // Query the books in the collection with a filter and an option.
-    // let filter = doc! { "author": "John Steinbeck" };
-    // let find_options = FindOptions::builder().sort(doc! { "title": 1 }).build();
-    // let mut cursor = collection.find(filter, find_options).await.unwrap();
+    Query the books in the collection with a filter and an option.
+    let filter = doc! { "author": "John Steinbeck" };
+    let find_options = FindOptions::builder().sort(doc! { "title": 1 }).build();
+    let mut cursor = collection.find(filter, find_options).await.unwrap();
 
-    // // Iterate over the results of the cursor.
-    // while let Some(book) = cursor.try_next().await.unwrap() {
-    //     println!("Book data: {:?}", book);
-    // }
+    // Iterate over the results of the cursor.
+    while let Some(book) = cursor.try_next().await.unwrap() {
+        println!("Book data: {:?}", book);
+    }
 
-    // custom_print("Finished querying the database!");
+    custom_print("Finished querying the database!");
 
     for i in 0..5 {
         // Perform operations that work with directly our model.
