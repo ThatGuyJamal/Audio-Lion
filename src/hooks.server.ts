c2ab4b171664c4e75f1213db44329341d8728d04
ThatGuyJamal <jamallyons8@gmail.com>
2023-03-29 00:38:15 -0400

    internal server setup

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,3 +6,27 @@ export const handleError = (({ error, event }: any) => {
     code: error?.code ?? "UNKNOWN",
   };
 }) satisfies HandleServerError;
+
+import Hapi from "@hapi/hapi";
+import getPort from "get-port";
+
+const init = async () => {
+	const toPortOrNotToPort = await getPort({ port: [6969, 9696] });
+
+	const server = Hapi.server({
+		port: toPortOrNotToPort,
+		host: "localhost",
+	});
+
+	await server.start();
+	console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+	console.log(err);
+	process.exit(1);
+});
+
+init();
+
+// make class to handle api requests and responses. Will be used by the svelte store states to manage the data.
