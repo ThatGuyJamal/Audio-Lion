253c66121c8af8256765277d84d1c4eb8d2f3fb4
ThatGuyJamal <jamallyons8@gmail.com>
2023-03-31 20:11:17 -0400

    fixed

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,4 +1,11 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+	export let result: PageData;
+	console.log(result);
+</script>
 
-<h1>Ok: {result.ok}</h1>
-<div>Message: {@html result.message}</div>
-<div>Data: {result.data}</div>
+<main>
+	<h1>Ok: {result.ok}</h1>
+	<div>Message: {result.message}</div>
+	<div>Data: {result.data}</div>
+</main>
